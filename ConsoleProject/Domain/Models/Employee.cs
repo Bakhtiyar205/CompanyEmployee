83c452185eaee178d45127788c6b729d5574c0eb
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2021-12-23 12:40:33 +0400

    Domain Repository Service

---
+++ ConsoleProject/Domain/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Employee : BaseEntity
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+}
---
+++ ConsoleProject/Service/Services/CompanyService.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Models;
+using Repository.Implementations;
+using Service.Services.InterFaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private CompanyRepository _CompanyRepository;
+        public CompanyService()
+        {
+            _CompanyRepository = new CompanyRepository();
+        }
+        public Company Create(Company model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Company model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Get(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(int id, Company model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
